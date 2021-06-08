5e05b317b63706e175c7d15ce33d1cf4e7690267
Kleber Moreti <kleber.camargo@fatec.sp.gov.br>
2021-06-08 22:14:32 +0100

    Updated

--- src/test/java/guru/sfg/beer/inventory/service/BeerInventoryApplicationTests.java
+++ src/test/java/guru/sfg/beer/inventory/service/BeerInventoryApplicationTests.java
@@ -3,7 +3,12 @@ package guru.sfg.beer.inventory.service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(
+        webEnvironment = RANDOM_PORT,
+        properties = {"spring.cloud.config.enabled=false"}
+)
 public class BeerInventoryApplicationTests {
 
     @Test

297616c2f6a157aa052c6f9e6acf9a38e3e45db2
Kleber Moreti <kleber.camargo@fatec.sp.gov.br>
2021-07-03 19:16:47 +0100

    progress...

--- target/generated-sources/annotations/guru/sfg/beer/inventory/service/web/mappers/BeerInventoryMapperImpl.java
+++ target/generated-sources/annotations/guru/sfg/beer/inventory/service/web/mappers/BeerInventoryMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-06-22T21:54:54+0100",
+    date = "2021-06-27T00:33:21+0100",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.11 (Ubuntu)"
 )
 @Component
